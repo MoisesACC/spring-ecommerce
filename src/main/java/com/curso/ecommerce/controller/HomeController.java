63307b539e5785faa33e84cdcf34053bc00ae906
MoisesACC <moisesaaroncc03@gmail.com>
2025-02-11 22:14:10 -0500

    redireccion al carrito de compras

--- src/main/java/com/curso/ecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/controller/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.curso.ecommerce.model.Producto;
@@ -40,6 +41,11 @@ public class HomeController {
         model.addAttribute("producto", producto);
 
         return "usuario/productohome";
+    }
+
+    @PostMapping("/card") // Añadir al carrito
+    public String addCard() { // Método para añadir al carrito
+        return "usuario/carrito"; // Redirige a la vista carrito
     }
 
 }
