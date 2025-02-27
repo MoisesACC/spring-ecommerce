636c5a3d1436c258b0e74c046b6d480d153b2d79
MoisesACC <moisesaaroncc03@gmail.com>
2025-02-26 21:08:24 -0500

    obtener usuario login y session

--- src/main/java/com/curso/ecommerce/controller/UsuarioController.java
+++ src/main/java/com/curso/ecommerce/controller/UsuarioController.java
@@ -1,5 +1,7 @@
 package com.curso.ecommerce.controller;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.curso.ecommerce.model.Usuario;
 import com.curso.ecommerce.service.IUsuarioService;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/usuario")
@@ -31,6 +35,32 @@ public class UsuarioController {
         logger.info("Usuario registro: {}", usuario);
         usuario.setTipo("USER");
         usuarioService.save(usuario);
+        return "redirect:/";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "usuario/login";
+    }
+
+    @PostMapping("/acceder")
+    public String acceder(Usuario usuario, HttpSession session) {
+        logger.info("Usuario login: {}", usuario);
+
+        Optional<Usuario> user = usuarioService.findByEmail(usuario.getEmail());
+        // logger.info("Usuario encontrado: {}", user.get());
+
+        if (user.isPresent()) {
+            session.setAttribute("idusuario", user.get().getId());
+            if (user.get().getTipo().equals("ADMIN")) {
+                return "redirect:/admin";
+            } else {
+                return "redirect:/";
+            }
+        } else {
+            logger.info("Usuario no encontrado");
+        }
+
         return "redirect:/";
     }
 }
