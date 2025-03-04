bec2b7dbe174c4a54f6c374db729d977300fb438
MoisesACC <moisesaaroncc03@gmail.com>
2025-03-03 20:56:05 -0500

    cerrar la sesion del usuario

--- src/main/java/com/curso/ecommerce/controller/UsuarioController.java
+++ src/main/java/com/curso/ecommerce/controller/UsuarioController.java
@@ -97,4 +97,9 @@ public class UsuarioController {
         return "usuario/detallecompra";
     }
 
+    @GetMapping("/cerrar")
+    public String cerrarSesion(HttpSession session) {
+        session.removeAttribute("idusuario");
+        return "redirect:/";
+    }
 }
